4d117ac53836dfea2cedae542ead0efb3115ce26
Mohamed Ahmed Attia Abdalla Elhadary <114479685+Hadary2402@users.noreply.github.com>
2024-04-23 12:40:34 +0200

    Bug Fixing Of systems messages being encrypted

--- client.cpp
+++ client.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <iomanip> // Include <iomanip> for setw and setfill
+#include <iomanip> 
 #include <cstring>
 #include <thread>
 #include <signal.h>
@@ -15,7 +15,9 @@
 #define MAX_LEN 200
 #define NUM_COLORS 6
 
-using namespace std; // Add using namespace std; to resolve the scope issue
+using namespace std;
+
+// Global variables
 char name[MAX_LEN];
 bool exit_flag = false;
 thread t_send, t_recv;
@@ -23,6 +25,7 @@ int client_socket;
 string def_col = "\033[0m";
 string colors[] = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
 
+// Function prototypes
 void catch_ctrl_c(int signal);
 string color(int code);
 int eraseText(int cnt);
@@ -30,8 +33,11 @@ void send_message(int client_socket);
 void recv_message(int client_socket);
 bool signup();
 bool login();
+string hashPassword(const string &password);
+void writeUserData(const string &name, const string &hashedPassword);
+bool readUserData(const string &name, string &hashedPassword);
 
-// Function to hash a password using SHA256
+// Hashes the password using SHA-256
 string hashPassword(const string &password) {
     EVP_MD_CTX *mdctx;
     const EVP_MD *md;
@@ -47,12 +53,12 @@ string hashPassword(const string &password) {
 
     stringstream ss;
     for (unsigned int i = 0; i < hashLen; i++) {
-        ss << hex << setw(2) << setfill('0') << (int)hash[i]; // Use setw and setfill here
+        ss << hex << setw(2) << setfill('0') << (int)hash[i]; 
     }
     return ss.str();
 }
 
-// Function to write user data to a file
+// Writes user data to a file
 void writeUserData(const string &name, const string &hashedPassword) {
     ofstream outfile("userdata.txt", ios::app);
     if (outfile.is_open()) {
@@ -63,7 +69,7 @@ void writeUserData(const string &name, const string &hashedPassword) {
     }
 }
 
-// Function to read user data from a file
+// Reads user data from a file
 bool readUserData(const string &name, string &hashedPassword) {
     ifstream infile("userdata.txt");
     string line;
@@ -80,32 +86,35 @@ bool readUserData(const string &name, string &hashedPassword) {
     return false;
 }
 
+// Main function
 int main() {
+    // Creating a socket
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket: ");
         exit(EXIT_FAILURE);
     }
 
+    // Configuring server address
     struct sockaddr_in client;
     client.sin_family = AF_INET;
     client.sin_port = htons(8888); // Port no. of server
     client.sin_addr.s_addr = INADDR_ANY;
-    //client.sin_addr.s_addr = inet_addr("127.0.0.1"); // Provide IP address of server
     memset(&client.sin_zero, 0, sizeof(client.sin_zero));
 
+    // Connecting to server
     if (connect(client_socket, reinterpret_cast<struct sockaddr *>(&client), sizeof(struct sockaddr_in)) == -1) {
         perror("connect: ");
         exit(EXIT_FAILURE);
     }
     signal(SIGINT, catch_ctrl_c);
 
-    // Signup and login
+    // Authentication loop
     bool loggedIn = false;
     while (!loggedIn) {
         cout << "Choose an option:\n1. Sign Up\n2. Log In\nYour choice: ";
         int choice;
         cin >> choice;
-        cin.ignore(); // Ignore newline character left in the input stream
+        cin.ignore(); 
         switch (choice) {
             case 1:
                 if (signup()) {
@@ -126,9 +135,7 @@ int main() {
         }
     }
 
-
-    // cout << colors[NUM_COLORS - 1] << "\n\t  <><><>Welcome back " << name << "!" << endl << def_col;
-
+    // Starting send and receive threads
     thread t1(send_message, client_socket);
     thread t2(recv_message, client_socket);
 
@@ -143,6 +150,7 @@ int main() {
     return 0;
 }
 
+// Signal handler for Ctrl+C
 void catch_ctrl_c(int signal) {
     char str[MAX_LEN] = "#exit";
     send(client_socket, str, sizeof(str), 0);
@@ -153,10 +161,12 @@ void catch_ctrl_c(int signal) {
     exit(signal);
 }
 
+// Returns ANSI color escape sequence
 string color(int code) {
     return colors[code % NUM_COLORS];
 }
 
+// Erases text from the console
 int eraseText(int cnt) {
     char back_space = 8;
     for (int i = 0; i < cnt; i++) {
@@ -165,13 +175,14 @@ int eraseText(int cnt) {
     return 0;
 }
 
+// Sends messages to the server
 void send_message(int client_socket) {
     while (true) {
         cout << colors[1] << "You : " << def_col;
         char str[MAX_LEN];
         cin.getline(str, MAX_LEN);
         
-        // Encrypt outgoing message using Caesar cipher with key 3
+        // Encrypting the message
         for (int i = 0; str[i] != '\0'; ++i) {
             if (isalpha(str[i])) {
                 str[i] = (str[i] + 3 - 'a') % 26 + 'a';
@@ -188,6 +199,7 @@ void send_message(int client_socket) {
     }
 }
 
+// Receives messages from the server
 void recv_message(int client_socket) {
     while (true) {
         if (exit_flag)
@@ -200,24 +212,31 @@ void recv_message(int client_socket) {
         recv(client_socket, &color_code, sizeof(color_code), 0);
         recv(client_socket, str, sizeof(str), 0);
             
-        for (int i = 0; str[i] != '\0'; ++i) {
-            if (isalpha(str[i])) {
-                char base = islower(str[i]) ? 'a' : 'A';
-                str[i] = (str[i] - 3 - base + 26) % 26 + base;
+        // Checking if the message is a system message or a user message
+        bool isSystemMessage = (strcmp(name, "#NULL") == 0);
+        
+        // Decrypting user messages only
+        if (!isSystemMessage) {
+            for (int i = 0; str[i] != '\0'; ++i) {
+                if (isalpha(str[i])) {
+                    char base = islower(str[i]) ? 'a' : 'A';
+                    str[i] = (str[i] - 3 - base + 26) % 26 + base;
+                }
             }
         }
         
+        // Displaying messages
         eraseText(6);
-        if (strcmp(name, "#NULL") != 0)
+        if (!isSystemMessage)
             cout << color(color_code) << name << " : " << def_col << str << endl;
         else
             cout << color(color_code) << str << endl;
-            cout << colors[1] << "You : " << def_col;
+        cout << colors[1] << "You : " << def_col;
         fflush(stdout);
     }
 }
 
-
+// User signup function
 bool signup() {
     string name, password;
     cout << "Enter name: ";
@@ -226,9 +245,10 @@ bool signup() {
     cin >> password;
     string hashedPassword = hashPassword(password);
     writeUserData(name, hashedPassword);
-    return true; // Assuming signup always succeeds for simplicity
+    return true; 
 }
 
+// User login function
 bool login() {
     string password;
     char name[MAX_LEN];
